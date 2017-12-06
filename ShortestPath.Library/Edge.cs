cb7a24d3a73b489f8e795002b0303666a962721c
Amirhossein <amirhosseinghorbani7@gmail.com>
2017-12-06 07:28:10 -0800

    Commenting in the edge class and explaining the details.

--- ShortestPath.Library/Edge.cs
+++ ShortestPath.Library/Edge.cs
@@ -1,8 +1,11 @@
 ﻿namespace ShortestPath.Library
 {
+    //An edge is a pointer to a vertex
     public struct Edge
     {
-        public string To { get; set; }
+        //To property store the name of the vertex which it's pointing to
+        public string To { get; set; } 
+        //Weight property store the weight of the edge.
         public long Weight { get; set; }
     }
 }
